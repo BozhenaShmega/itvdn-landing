0fb708fb93b56a061f9b622c2e0215c96bb0eb72
Bozhena Shmega <bozhenabee@gmail.com>
2018-06-20 14:32:44 +0300

    README was corrected

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
 
     var openFormButton = document.querySelector('.arrow-down');
     var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
 
     if (openFormButton) {
         openFormButton.addEventListener('click', function(e) {
@@ -20,4 +21,18 @@
             }
         })
     }
+
+    if (nav) {
+        nav.addEventListener('click', function(e) {
+            var target = e.target;
+
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+
+            e.preventDefault();
+            ITVDN.navigation.toggleToActiveLink(target);
+        });
+    }
+
 }());
